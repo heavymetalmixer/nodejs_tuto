f033e48abdf8390df6de2c462e99ea5b11ef4b84
heavymetalmixer <heavymetalmixer@gmail.com>
2025-06-09 14:27:41 -0500

    Added .gitignore file and other stuff

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,5 +1,7 @@
 const { readFileSync, writeFileSync } = require("fs");
 
+console.log("Start!");
+
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
 
@@ -8,3 +10,6 @@ writeFileSync(
   `Here is the result: ${first}, ${second}`,
   { flag: 'a' } // 'a' for append
 )
+
+console.log("Done with this task.");
+console.log("Starting the next one. . .");
